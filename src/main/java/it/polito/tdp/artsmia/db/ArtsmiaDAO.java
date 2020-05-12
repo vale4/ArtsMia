1649020559dee3f35ff82c67be535184ad8749a3
Maco <Maco@LAPTOP-O1JLL8OB>
2020-05-12 21:27:18 +0200

    Svolti i punti a e b del PUNTO 1

--- src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
+++ src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
@@ -6,36 +6,83 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import it.polito.tdp.artsmia.model.Adiacenza;
 import it.polito.tdp.artsmia.model.ArtObject;
 
 public class ArtsmiaDAO {
 
-	public List<ArtObject> listObjects() {
+	public void listObjects(Map<Integer, ArtObject>idMap) {
 		
 		String sql = "SELECT * from objects";
-		List<ArtObject> result = new ArrayList<>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
+				
+				if(!idMap.containsKey(res.getInt("object_id"))) {
 
 				ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
 						res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
 						res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
 						res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
+				idMap.put(artObj.getId(), artObj);
+				}
 				
-				result.add(artObj);
 			}
 			conn.close();
-			return result;
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public int getPeso(ArtObject a1, ArtObject a2) {
+		String sql="SELECT COUNT(*) as peso " + 
+				"FROM exhibition_objects AS eo1, exhibition_objects as eo2 " + 
+				"WHERE eo1.exhibition_id=eo2.exhibition_id AND eo1.object_id=?  AND eo2.object_id=? ";
+		Connection conn = DBConnect.getConnection();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, a1.getId());
+			st.setInt(2, a2.getId());
+			ResultSet res = st.executeQuery();
+			if(res.next()) {
+				int peso=res.getInt("peso");
+				conn.close();
+				return peso;
+			}
+			
+			conn.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	
+	public List<Adiacenza>getAdiacenze(){
+		String sql="SELECT eo1.object_id as obj1, eo2.object_id as obj2, COUNT(*) as peso " + 
+				"FROM exhibition_objects AS eo1, exhibition_objects as eo2 " + 
+				"WHERE eo1.exhibition_id=eo2.exhibition_id AND eo1.object_id>eo2.object_id " + 
+				"GROUP BY eo1.object_id, eo2.object_id ";
+		Connection conn = DBConnect.getConnection();
+		List<Adiacenza>adiacenze=new ArrayList<Adiacenza>();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while(res.next()) {
+				adiacenze.add(new Adiacenza(res.getInt("obj1"), res.getInt("obj2"), res.getInt("peso")));
+			}
+			conn.close();
+			return adiacenze;
+		}catch (SQLException e) {
+			e.printStackTrace();
 			return null;
 		}
+		
 	}
 	
 }
